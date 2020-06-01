b96f9aa2773bb8ce43d594a7539dd47789984921
Mickael <noxbike@gmail.com>
2020-06-01 15:36:39 +0000

    bar de recherche ajouté avec animation

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 $(function(){
-    
+    $('#search').one('click', function(){
+            console.log('clicked');
+        $('<input>', {class: 'barSearch', placeholder: 'search'}).appendTo('#search');
+
+        $('.barSearch').animate({opacity: '1', width: '100%'}, 500, 'linear');
+    })
 })
