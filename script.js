d2dc1293f1c5cfb11ff69775db935c93288023c7
mickael morel <noxbike@gmail.com>
2020-06-02 22:07:44 +0000

    responsive smartphone ajouter

--- script.js
+++ script.js
@@ -5,4 +5,16 @@ $(function(){
 
         $('.barSearch').animate({opacity: '1', width: '100%'}, 500, 'linear');
     })
+
+    $('.menu-deroulant').click(function(){
+        $('#menu').show();
+        $('.menu-deroulant').hide();
+        $('#close-menu').show();
+    })
+
+    $('#close-menu').click(function(){
+        $('#menu').hide();
+        $('.menu-deroulant').show();
+        $('#close-menu').hide();
+    })
 })
